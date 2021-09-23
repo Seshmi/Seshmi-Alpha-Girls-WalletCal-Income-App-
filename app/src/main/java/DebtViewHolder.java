ef07b7d416daa418973933d386304c0f8e6453d7
niwarthana98 <it20035358@my.sliit.lk>
2021-09-23 20:16:00 +0530

    Add the debtview Hoder file

---
+++ app/src/main/java/DebtViewHolder.java
@@ -0,0 +1,26 @@
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DebtViewHolder extends RecyclerView.ViewHolder {
+
+
+    TextView name,datedue,amount,dis;
+    LinearLayout debtlay;
+    public DebtViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        name = itemView.findViewById(R.id.debt_name);
+        datedue = itemView.findViewById(R.id.datedue);
+        amount = itemView.findViewById(R.id.amount_debt);
+        dis = itemView.findViewById(R.id.debt_dis);
+        debtlay = itemView.findViewById(R.id.debtview);
+
+
+
+    }
+}
